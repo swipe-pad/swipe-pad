a1f5c37f0f10d22ed1865a6b92a584aac76ae708
Otto G <5030059+ottodevs@users.noreply.github.com>
2026-02-03 10:51:48 +0000

    🔧 chore(next): update configuration with experimental features

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  reactCompiler: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
