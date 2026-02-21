e207186a1be1d3152ddc20da2fa8e2e4895b5c8c
Otto G <5030059+ottodevs@users.noreply.github.com>
2026-02-21 23:19:19 +0100

    🔧 chore(config): update project configuration and editor settings

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,42 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "app.hypercerts.org",
+      },
+      {
+        protocol: "https",
+        hostname: "hypercerts.org",
+      },
+      {
+        protocol: "https",
+        hostname: "images.weserv.nl",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "imagedelivery.net",
+      },
+      {
+        protocol: "https",
+        hostname: "production-karma-gap-projects.s3.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.celorean.school",
+      },
+      {
+        protocol: "https",
+        hostname: "bafybeihaqyxy6vrelryd66eewjggu3tafuv2hfof6cmzzga2qpfmd4gmue.ipfs.w3s.link",
+      },
+      {
+        protocol: "https",
+        hostname: "learn.tg",
+      },
     ],
   },
   reactCompiler: !isDev,
